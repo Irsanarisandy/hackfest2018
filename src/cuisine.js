3507e9865bbe3043e9076e593f915240188e62a1
JlewisPk <lewis4692@hotmail.com>
2018-08-19 13:52:51 +1200

    processing displaying data to final page

--- src/cuisine.js
+++ src/cuisine.js
@@ -23,7 +23,7 @@ class HTML {
 // Variables
 const cuisineButtonOne = document.getElementById('#button1');
 // saves restaurants data
-var entries;
+var entries = [];
 // Instantiate the HTML Class
 const html = new HTML();
 
@@ -51,9 +51,58 @@ function getRestaurantData(cuisineType){
         .then(response => response.text())
         .then(data => JSON.parse(data))
         .then(data => {
-            console.log('version:', data.zenbu.version);
-            entries = data.zenbu.entries.entry
+            // entries.push(data.zenbu.entries.entry)
+            for (i = 0; i < data.zenbu.entries.entry.length; i++) { 
+                entries.push(data.zenbu.entries.entry[i])
+            }
+
+            console.log('array:', entries);
+            // $("restaurantName").update(entries[0].name)
+            // document.getElementById("restaurantName").innerHTML = entries[0].name;
+            localStorage.setItem("entries", JSON.stringify(entries))
+            localStorage.setItem("cuisineType", JSON.stringify(cuisineType))
+        })
+        .then(()=> {
+            window.location.href = 'http://localhost:5000/lastPage.html'
         })
 }
 
+function loadRestaurantData() {
+    entries = JSON.parse(localStorage.getItem("entries"));
+    var cuisineType = JSON.parse(localStorage.getItem("cuisineType"));
+    console.log(entries[0].name)
+    document.getElementById("restaurantName").innerHTML = entries[0].name;
+    document.getElementById("cuisineType").innerHTML = cuisineType;
+    document.getElementById("placeAddress").innerHTML = entries[0].address;
+    document.getElementById("placePhone").innerHTML = entries[0].phone;
+    if (typeof entries[0].open === 'object') {
+        console.log('no data');
+
+    document.getElementById("placeHours").innerHTML = '( Please check with the restaurant )';
+    } else {
+        document.getElementById("placeHours").innerHTML = entries[0].open;
+    }
+
+    localStorage.setItem("entriesIndex", 0)
+}
+
+function loopRestaurants() {
+    var entriesIndex = JSON.parse(localStorage.getItem("entriesIndex"));
+    entriesIndex = entriesIndex + 1;
+    var cuisineType = JSON.parse(localStorage.getItem("cuisineType"));
+    
+    document.getElementById("restaurantName").innerHTML = entries[entriesIndex].name;
+    document.getElementById("cuisineType").innerHTML = cuisineType;
+    document.getElementById("placeAddress").innerHTML = entries[entriesIndex].address;
+    document.getElementById("placePhone").innerHTML = entries[entriesIndex].phone;
+    if (typeof entries[0].open === 'object') {
+        console.log('no data');
+
+    document.getElementById("placeHours").innerHTML = '( Please check with the restaurant )';
+    } else {
+        document.getElementById("placeHours").innerHTML = entries[entriesIndex].open;
+    }
+
+    localStorage.setItem("entriesIndex", entriesIndex)
+}
 
